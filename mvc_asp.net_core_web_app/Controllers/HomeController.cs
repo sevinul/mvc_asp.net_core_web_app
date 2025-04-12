4a32103b350f50586cc9878c9992f009ba555d9b
Sevin Ulukut <sevin.ulukut@online.de>
2025-04-12 07:20:47 +0200

    Projektdateien hinzufügen.

---
+++ mvc_asp.net_core_web_app/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc_asp.net_core_web_app.Models;
+
+namespace mvc_asp.net_core_web_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mvc_asp.net_core_web_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_asp.net_core_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
